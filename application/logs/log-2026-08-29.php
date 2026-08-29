4eddcf240a31550ca3d935338c8358223d703800
nihaan62 <nihaanshah062@gmail.com>
2026-08-29 17:04:00 +0530

    chore: add error log entry for failed database connection

---
+++ application/logs/log-2026-08-29.php
@@ -0,0 +1,3 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+
+ERROR - 2026-08-29 13:21:09 --> Severity: error --> Exception: No connection could be made because the target machine actively refused it C:\xampp\htdocs\perfex_crm\system\database\drivers\mysqli\mysqli_driver.php 203
